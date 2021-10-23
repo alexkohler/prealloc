4b4ec6e4093af677ac299edbbd56587ee126aa5e
Ville Skyttä <ville.skytta@upcloud.com>
2021-10-23 10:49:38 +0300

    Add map preallocation benchmarks

--- prealloc_test.go
+++ prealloc_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go/token"
 	"testing"
 
@@ -123,5 +124,36 @@ func BenchmarkSize200PreallocateCopy(b *testing.B) {
 		// Preallocate our initial slice
 		init := make([]int64, len(existing))
 		copy(init, existing)
+	}
+}
+
+func BenchmarkMap(b *testing.B) {
+	benchmarks := []struct {
+		size        int
+		preallocate bool
+	}{
+		{10, false},
+		{10, true},
+		{200, false},
+		{200, true},
+	}
+	var m map[int]int
+	for _, bm := range benchmarks {
+		no := ""
+		if !bm.preallocate {
+			no = "No"
+		}
+		b.Run(fmt.Sprintf("Size%d%sPreallocate", bm.size, no), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if bm.preallocate {
+					m = make(map[int]int, bm.size)
+				} else {
+					m = make(map[int]int)
+				}
+				for j := 0; j < bm.size; j++ {
+					m[j] = j
+				}
+			}
+		})
 	}
 }
