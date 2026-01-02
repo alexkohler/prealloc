a1b7e12c1e319df41e07c26cd1114d67281062c0
Nathan Baulch <nathan.baulch@gmail.com>
2026-01-03 09:58:31 +1100

    Exclude nested loops dependent on outer loop vars

--- testdata/nested.go
+++ testdata/nested.go
@@ -26,10 +26,56 @@ func nestedBlocks() {
 	}
 }
 
-func nestedLoops() {
+func nestedRangeIndependent() {
 	var x []int // want "Consider preallocating x with capacity 50$"
 	for i := range "Hello" {
 		for j := range "Hello" {
+			x = append(x, i, j)
+		}
+	}
+}
+
+func nestedRangeDependentKey() {
+	var x []int
+	for i := range "Hello" {
+		for j := range i {
+			x = append(x, i, j)
+		}
+	}
+}
+
+func nestedRangeDependentValue() {
+	var x []int
+	for i, v := range "Hello" {
+		for j := range v {
+			x = append(x, i, int(j))
+		}
+	}
+}
+
+func nestedRangeDependentNewVar() {
+	var x []int
+	for i := range "Hello" {
+		ii := i
+		for j := range ii {
+			x = append(x, ii, j)
+		}
+	}
+}
+
+func nestedForDependentLower() {
+	var x []int
+	for i := 0; i < 10; i++ {
+		for j := i; j < 10; j++ {
+			x = append(x, i, j)
+		}
+	}
+}
+
+func nestedForDependentUpper() {
+	var x []int
+	for i := 0; i < 10; i++ {
+		for j := 0; j < i; j++ {
 			x = append(x, i, j)
 		}
 	}
